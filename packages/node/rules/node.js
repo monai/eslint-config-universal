927e5c0ddccaa2820ce1d2d9b81b9fc349dd7378
monai <monai@cure.lt>
2022-02-10 17:00:55 +0200

    Allow generators is node

--- packages/node/rules/node.js
+++ packages/node/rules/node.js
@@ -1,11 +1,31 @@
 'use strict';
 
 module.exports = {
+  parserOptions: {
+    ecmaFeatures: {
+      generators: true,
+    },
+  },
   extends: [
     'plugin:node/recommended',
     'airbnb-base/rules/node',
   ],
   rules: {
+    'no-restricted-syntax': [
+      'error',
+      {
+        selector: 'ForInStatement',
+        message: 'for..in loops iterate over the entire prototype chain, which is virtually never what you want. Use Object.{keys,values,entries}, and iterate over the resulting array.',
+      },
+      {
+        selector: 'LabeledStatement',
+        message: 'Labels are a form of GOTO; using them makes code confusing and hard to maintain and understand.',
+      },
+      {
+        selector: 'WithStatement',
+        message: '`with` is disallowed in strict mode because it makes code impossible to predict and optimize.',
+      },
+    ],
     'no-use-before-define': ['error', { functions: false }],
   },
 };
